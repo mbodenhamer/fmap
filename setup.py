7762772298e32da1f1b33cd22169ecbc6597ee85
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-03-29 14:49:31 -0500

    Adding classifiers

--- setup.py
+++ setup.py
@@ -23,5 +23,19 @@ setup(
         ]
     },
     license = 'MIT',
-    classifiers = []
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities'
+    ]
 )
