d028c54a52dbd7bcf8591f6069f49d515f8f2cca
mbodenhamer <mbodenhamer@mbodenhamer.com>
2016-03-29 19:03:51 -0500

    Adding unit tests

---
+++ tests/unit_tests.py
@@ -0,0 +1,28 @@
+from fmap._fmap import excluded, applicable
+
+#-------------------------------------------------------------------------------
+# Exclusion/Inclusion Utilities
+
+def test_excluded():
+    excludes = ['*a', '*b']
+    names = ['a', 'b', 'c', 'ca', 'cb', 'ac', 'ab', 'cd', 'e']
+    assert list(excluded(names, excludes)) == ['a', 'b', 'ca', 'cb', 'ab']
+
+def test_applicable():
+    excludes = ['*a', '*b']
+    names = ['a', 'b', 'c', 'ca', 'cb', 'ac', 'ab', 'cd', 'e']
+    
+    patterns = ['*']
+    assert list(applicable(names, patterns, excludes)) == ['c', 'ac', 'cd', 'e']
+
+    patterns = ['*d']
+    assert list(applicable(names, patterns, excludes)) == ['cd']
+
+    patterns = ['*d', '*c']
+    assert list(applicable(names, patterns, excludes)) == ['c', 'ac', 'cd']
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    test_excluded()
+    test_applicable()
